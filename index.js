cf84c8783029c053a28be5b2a95066d686e9a51a
Alex McCleery <mccleeryalex@gmail.com>
2026-09-18 05:46:48 -0400

    isColdAndHot() returns correct boolean and passes all tests

--- index.js
+++ index.js
@@ -123,6 +123,10 @@ export function shareLastDigit(num1, num2) {
  */
 export function isColdAndHot(temp1, temp2) {
   /* TODO */
+  if (temp1 > 100 && temp2 < 0 || temp1 < 0 && temp2 > 100) {
+    return true;
+  }
+  return false;
 }
 
 /**
